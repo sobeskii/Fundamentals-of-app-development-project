72ca8e9f39acd06a5534a14582cf9bae7316f8eb
Auguste <auguste.vir@gmail.com>
2021-12-06 15:38:58 +0200

    Final design touches added

--- app/src/main/java/ktu/edu/projektas/app/ui/user/NotificationFragment.kt
+++ app/src/main/java/ktu/edu/projektas/app/ui/user/NotificationFragment.kt
@@ -8,44 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import com.facebook.CallbackManager
-import com.facebook.FacebookSdk
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import ktu.edu.projektas.app.data.ScheduleViewModel
 import ktu.edu.projektas.app.data.ScheduleViewModelFactory
 import ktu.edu.projektas.app.data.User
-import ktu.edu.projektas.app.ui.home.HomeAdapter
-import ktu.edu.projektas.app.utils.formatLocalDateTime
-import ktu.edu.projektas.app.utils.longToLocalDateTime
-import ktu.edu.projektas.databinding.FragmentHomeBinding
 import ktu.edu.projektas.databinding.FragmentNotificationBinding
 import android.content.Intent
-import android.net.Uri
-import com.facebook.share.model.ShareContent
-import com.facebook.share.model.ShareLinkContent
-import com.facebook.share.model.ShareMediaContent
-import com.facebook.share.model.SharePhoto
-import android.R
-import android.content.ContentValues.TAG
-import android.util.Log
-
-import com.facebook.share.widget.ShareButton
-
-import com.facebook.share.Sharer
-
-import com.facebook.FacebookCallback
-import com.facebook.share.widget.ShareDialog
-import com.facebook.FacebookException
-
-
-
-
-
-
-
-
-
 
 class NotificationFragment : Fragment() {
 
@@ -62,9 +31,6 @@ class NotificationFragment : Fragment() {
         ScheduleViewModelFactory(requireContext())
     }
 
-
-
-
     override fun onCreateView(
 
         inflater: LayoutInflater, container: ViewGroup?,
@@ -73,20 +39,15 @@ class NotificationFragment : Fragment() {
         mAuth = FirebaseAuth.getInstance()
         binding = FragmentNotificationBinding.inflate(inflater, container, false)
 
-
-
-
-
         binding.button2.setOnClickListener{
-            var message = "COVID OUTBREAK RECOGNIZED"
-            var share : Intent = Intent(Intent.ACTION_SEND);
-            share.setType("text/plain");
+            val message = "COVID case detected in the event!"
+            val share : Intent = Intent(Intent.ACTION_SEND);
+            share.type = "text/plain";
             share.putExtra(Intent.EXTRA_TEXT, message);
 
-            startActivity(Intent.createChooser(share, "Title of the dialog the system will open"))
+            startActivity(Intent.createChooser(share,
+                "Title of the dialog the system will open"))
         }
-
-
 
         adapter = NotificationAdapter()
 
@@ -100,16 +61,8 @@ class NotificationFragment : Fragment() {
         binding.notificationAdapter.adapter = adapter
         binding.lifecycleOwner = viewLifecycleOwner
 
-
-
-
-
         return binding.root
     }
-
-
-
-
 
     private fun setVisible(boolean: Boolean){
         binding.notificationAdapter.visibility = if(boolean) View.VISIBLE else View.GONE
